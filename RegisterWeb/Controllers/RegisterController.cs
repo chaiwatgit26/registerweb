25bd72033120857fb549c67883ec18877b351334
chaiwatgit26 <chaiwat020817@gmail.com>
2026-09-21 15:08:45 +0700

    Create Controller for Register

---
+++ RegisterWeb/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterWeb.Repositories;
+
+namespace RegisterWeb.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly OccupationRepository _occupationRepository;
+
+        public RegisterController(
+            OccupationRepository occupationRepository)
+        {
+            _occupationRepository = occupationRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var occupations = _occupationRepository.GetAll();
+
+            return View(occupations);
+        }
+    }
+}
